ec54ba9b61d6d954706fe18b6d01d0ac3bbda1d8
Adéla Krásová <>
2022-03-20 09:02:44 +0100

    bonus homework finished

--- vyvolavac.js
+++ vyvolavac.js
@@ -46,9 +46,39 @@ function tahnoutJmeno() {
     // Výherní jméno si uložíme do pole k ostatním výherním
     tazenaJmena.push(vyherniJmeno);
 
+    //vypíše výherní jméno
     let vyherce = document.querySelector('#vyherka');
     vyherce.innerHTML = vyherniJmeno;
 
+    // vypíše seznam dosud tažených jmen
     let seznamJmen = document.querySelector('#seznam');
     seznamJmen.innerHTML = tazenaJmena;
+
+    // vypíše počet již tažených jmen číslem
+    let pocetVyvolanych = tazenaJmena.length;
+    let pocetVyvolanychDiv = document.querySelector('#pocet-vyvolanych')
+    pocetVyvolanychDiv.innerHTML = pocetVyvolanych;
+
+    console.log(jmena);
+};
+
+function resetJmena() {
+
+    // Přidáme TazenaJmena zpátky do Jmena
+    jmena = jmena.concat(tazenaJmena);
+
+    // Vyprazdnime TazenaJmena
+    tazenaJmena = [];
+
+    // Vyprázdnění vepsaných dat na stránce:
+    let vyherce = document.querySelector('#vyherka');
+    vyherce.innerHTML = "";
+
+    let seznamJmen = document.querySelector('#seznam');
+    seznamJmen.innerHTML = "Seznam vyvolaných";
+
+    let pocetVyvolanychDiv = document.querySelector('#pocet-vyvolanych')
+    pocetVyvolanychDiv.innerHTML = "Dosud žádná";
+    
+console.log(jmena);
 }
